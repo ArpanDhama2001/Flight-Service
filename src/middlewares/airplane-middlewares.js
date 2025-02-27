612aaa44ed3291ead05854cdbc5e3f06a6a9b947
Arpan Dhama <arpandhama2001@gmail.com>
2025-02-27 19:54:32 +0530

    Initial setup with one CRUD API done

---
+++ src/middlewares/airplane-middlewares.js
@@ -0,0 +1,17 @@
+const { StatusCodes } = require("http-status-codes");
+const AppError = require("../utils/errors/app-error");
+const { ErrorResponse } = require("../utils/common");
+
+function validateCreateRequest(req, res, next) {
+    if (!req.body.modelNumber) {
+        ErrorResponse.message = "Something went wrong while creating Airplane.";
+        ErrorResponse.error = new AppError(
+            ["modelNumber is not found in request body."],
+            StatusCodes.BAD_REQUEST
+        );
+        return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+    }
+    next();
+}
+
+module.exports = { validateCreateRequest };
---
+++ src/services/airplane-service.js
@@ -0,0 +1,22 @@
+const { StatusCodes } = require("http-status-codes");
+const { AirplaneRepository } = require("../repositories");
+const AppError = require("../utils/errors/app-error");
+const airplaneRepository = new AirplaneRepository();
+
+async function createAirplane(data) {
+    try {
+        const airplane = await airplaneRepository.create(data);
+        return airplane;
+    } catch (error) {
+        if ((error.name = "SequelizeValidationError")) {
+            let explanation = [];
+            error.errors.forEach((e) => {
+                explanation.push(e.message);
+            });
+            throw new AppError(explanation, StatusCodes.BAD_REQUEST);
+        }
+        throw error;
+    }
+}
+
+module.exports = { createAirplane };
