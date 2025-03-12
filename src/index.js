96b92da85e0add2bddf3cb6138f22cd3fd22a9b1
Arpan Dhama <arpandhama2001@gmail.com>
2025-03-12 18:02:16 +0530

    showcased association methods of sequelize

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes");
+const { where } = require("sequelize");
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,21 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Server is listening on port ${ServerConfig.PORT}`);
+
+    // bad code
+    const { City } = require("./models");
+    const city = await City.findByPk(14);
+    // console.log(city);
+    const ccu = city.createAirport({
+        name: "Netaji Subhas Chandra Bose International Airport",
+        code: "CCU",
+    });
+    // console.log(ccu);
+    // await City.destroy({
+    //     where: {
+    //         id: 14,
+    //     },
+    // });
 });
