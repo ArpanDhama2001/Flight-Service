5c7ec2bdb61c636d8d4f9ccaf9f663aeaf2874f4
Arpan Dhama <arpandhama2001@gmail.com>
2025-03-12 18:53:50 +0530

    Added flights model with foreign key constraint defined during model definition ``` npx sequelize model:generate --name Flight --attributes flightNumber:string,airplaneId:integer,departureAirportId:integer,arrivalAirportId:integer,arrivalTime:date,departureTime:date,price:integer,boardingGate:string,totalSeats:integer ```

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes");
-const { where } = require("sequelize");
 
 const app = express();
 app.use(express.json());
@@ -9,21 +8,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRoutes);
 
-app.listen(ServerConfig.PORT, async () => {
+app.listen(ServerConfig.PORT, () => {
     console.log(`Server is listening on port ${ServerConfig.PORT}`);
-
-    // bad code
-    const { City } = require("./models");
-    const city = await City.findByPk(14);
-    // console.log(city);
-    const ccu = city.createAirport({
-        name: "Netaji Subhas Chandra Bose International Airport",
-        code: "CCU",
-    });
-    // console.log(ccu);
-    // await City.destroy({
-    //     where: {
-    //         id: 14,
-    //     },
-    // });
 });
