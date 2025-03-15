80160b4cb1b90d55dc49e338fc2c71d94990dd6b
Arpan Dhama <arpandhama2001@gmail.com>
2025-03-15 17:58:12 +0530

    FEAT: added getAllFlights API

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -2,6 +2,7 @@ const { StatusCodes } = require("http-status-codes");
 
 const { FlightRepository } = require("../repositories");
 const AppError = require("../utils/errors/app-error");
+const { Op } = require("sequelize");
 
 const flightRepository = new FlightRepository();
 
@@ -24,6 +25,62 @@ async function createFlight(data) {
     }
 }
 
+// api/v1/flights/?trips=MUM-DEL&travellers=2&price=3000-4000&sort=price_DESC,departureTime_ASC
+
+async function getAllFlights(query) {
+    let customFilter = {};
+    let sortFilter = [];
+    const endingTripTime = " 23:59:00";
+    // trips=MUM-DEL
+    if (query.trips) {
+        [departureAirportId, arrivalAirportId] = query.trips.split("-");
+        if (departureAirportId === arrivalAirportId) {
+            throw new AppError(
+                "Departure Airport and Arrival Airport can't be same",
+                StatusCodes.BAD_REQUEST
+            );
+        }
+
+        customFilter.departureAirportId = departureAirportId;
+        customFilter.arrivalAirportId = arrivalAirportId;
+    }
+    if (query.price) {
+        [minPrice, maxPrice] = query.price.split("-");
+        customFilter.price = {
+            [Op.between]: [minPrice, maxPrice == undefined ? 20000 : maxPrice],
+        };
+    }
+    if (query.travellers) {
+        customFilter.totalSeats = {
+            [Op.gte]: query.travellers,
+        };
+    }
+    if (query.tripDate) {
+        customFilter.departureTime = {
+            [Op.between]: [query.tripDate, query.tripDate + endingTripTime],
+        };
+    }
+    if (query.sort) {
+        const params = query.sort.split(",");
+        const sortFilters = params.map((param) => param.split("_"));
+        sortFilter = sortFilters;
+    }
+    console.log(customFilter, sortFilter);
+    try {
+        const flights = await flightRepository.getAllFlights(
+            customFilter,
+            sortFilter
+        );
+        return flights;
+    } catch (error) {
+        throw new AppError(
+            "Cannot fetch data of all the flights",
+            StatusCodes.INTERNAL_SERVER_ERROR
+        );
+    }
+}
+
 module.exports = {
     createFlight,
+    getAllFlights,
 };
